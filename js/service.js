cb2947ea0d23b4cee63d7345bd8959b52077e948
mcekov <mcekovphoto@gmail.com>
2019-01-22 09:31:58 +0200

    fix minor bug in validation

--- js/service.js
+++ js/service.js
@@ -4608,6 +4608,8 @@ let service = (() => {
     ev.preventDefault();
 
     const calcForm = getFormData($('#calcForm'));
+    console.log(calcForm.weight);
+    
     const country = $('#countrySelect option:selected').text();
     const zone = Number(calcForm.zone);
 
@@ -4617,7 +4619,7 @@ let service = (() => {
       showError('Please Enter Valid Weight!');
     } else  if (calcForm.fuelTax <= 0) {
       showError('Please Enter Valid Fuel Tax!');
-    } else if (calcForm.discount <= 0){
+    } else if (calcForm.discount < 0){
       showError('Please Enter Valid Weight!');
     } else {
     let price = getPrice(calcForm.weight, zone, calcForm.tariff);
